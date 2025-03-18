7fec797eda1d938abdf85e1277d50ba07f1ffeb0
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-03-18 18:02:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactarnos. Nos pondremos en contacto contigo pronto.');
+});
